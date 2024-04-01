cbbc9a2ee2a3d60fe5f738f9bb431940da58e0bc
zhuba-Ahhh <3477826311@qq.com>
2024-04-01 17:17:08 +0800

    feat: clock

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Clock from 'components/Clock';
+
+function App() {
+  return (
+    <Clock />
+  );
+}
+
+export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Clock: React.FC = () => {
+  const [time, setTime] = useState(new Date());
+  const minuteRef = useRef<HTMLDivElement>(null);
+  const hourRef = useRef<HTMLDivElement>(null);
+  const secondRef = useRef<HTMLDivElement>(null);
+  const timeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const tick = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(tick);
+  }, []);
+
+  useEffect(() => {
+    updateClockHands();
+  }, [time]);
+
+  const updateClockHands = () => {
+    if (!minuteRef.current || !hourRef.current || !secondRef.current || !timeRef.current) {
+      return;
+    }
+
+    const minuteDeg = 180 + (360 / 60) * time.getMinutes() + (360 / 3600) * time.getSeconds();
+    const secondDeg = 180 + (360 / 60) * time.getSeconds();
+    const hourDeg = 180 + (360 / 12) * time.getHours() + (360 / 720) * time.getMinutes();
+
+    minuteRef.current.style.transform = `rotateZ(${minuteDeg}deg)`;
+    hourRef.current.style.transform = `rotateZ(${hourDeg}deg)`;
+    secondRef.current.style.transform = `rotateZ(${secondDeg}deg)`;
+    const formattedTime = time.toLocaleTimeString('en-US', { hour12: true });
+    const [hour, minute, second] = formattedTime.split(':').map((part) => parseInt(part));
+
+    timeRef.current!.textContent = `${hour}:${minute.toString().padStart(2, '0')}:${second
+      .toString()
+      .padStart(2, '0')}`
+  };
+
+  return (
+    <div className="wrap">
+      <div className="clock-wrap">
+        <svg>
+          <circle cx="150" cy="150" r="150" />
+        </svg>
+        <div className="clock-root">
+          <div className="point" />
+        </div>
+        <div className="hand-wrap">
+          <div className="hand minute-hand" ref={minuteRef} />
+        </div>
+        <div className="hand-wrap">
+          <div className="hand second-hand" ref={secondRef} />
+        </div>
+        <div className="hand-wrap">
+          <div className="hand hour-hand" ref={hourRef} />
+        </div>
+        <div className="bottom-time" ref={timeRef} />
+      </div>
+    </div>
+  );
+};
+export default Clock;
